32c90a2f0b51ef534508585a798badc0aa277104
Christopher Black <chris@blacktopinteractive.com>
2019-02-03 21:30:54 -0600

    Added unit testing for *.js, *.html, a route and files that shouldn't be returned.

---
+++ test/server.test.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server/server');
+const chaiFiles = require('chai-files');
+const expect = chai.expect;
+const file = chaiFiles.file;
+
+chai.use(chaiFiles);
+chai.use(chaiHttp);
+
+describe('test main server file', function () {
+    it('should list ALL colors on /color GET', function (done) {
+        chai.request(server)
+            .get('/color')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+    it('should 404 on a random page', function (done) {
+        chai.request(server)
+            .get('/somerandomlocation')
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+    });
+    it('should correctly download a *.js file', function (done) {
+        chai.request(server)
+            .get('/scripts/test.js')
+            .end((err, res) => {
+                res.should.have.status(200);
+                expect(file('server/public/scripts/test.js')).to.equal(res.text);
+                done();
+            });
+    });
+    it('should correctly download a *.html file', function (done) {
+        chai.request(server)
+            .get('/index.html')
+            .end((err, res) => {
+                res.should.have.status(200);
+                expect(file('server/public/index.html')).to.equal(res.text);
+                done();
+            });
+    });
+    it('should NOT return files outside of public', function (done) {
+        chai.request(server)
+            .get('/../../README.md')
+            .end((err, res) => {
+                res.should.have.status(404);
+                expect(file('README.md')).to.not.equal(res.text);
+                done();
+            });
+    });
+});
